42bd5f7958f22a25e8429b7d7131642865080c43
Lugrim <Lugrim@users.noreply.github.com>
2023-04-06 16:55:28 +0200

    🐛 Added test user who can create databases

--- init_db.sql
+++ init_db.sql
@@ -1,6 +1,10 @@
 \set user_name `echo ${INSALAN_USERNAME}`
+\set test_user `echo ${INSALAN_USERNAME}_test`
 \set user_pass `echo ${INSALAN_PASSWORD}`
 \set db_name `echo ${INSALAN_DATABASE}`
+
+CREATE USER :test_user WITH PASSWORD :'user_pass';
+ALTER USER :test_user CREATEDB;
 
 CREATE USER :user_name WITH PASSWORD :'user_pass';
 CREATE DATABASE :db_name;
